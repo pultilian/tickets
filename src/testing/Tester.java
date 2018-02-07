ba27236e89f1ef2a935a713eba44c37936472fa5
Will Pinkston <william@pinkston.com>
2018-02-07 10:07:29 -0700

    Main class and method for basic testing

--- src/testing/Tester.java
+++ src/testing/Tester.java
@@ -1,7 +1,10 @@
 package testing;
 
-import 
+import communicators.ServerCommunicator;
 
 public class Tester {
-
+    public void main(String[] args) {
+        System.out.println("You're testing! Yay you.");
+        return;
+    }
 }
