ccc0d67fe08794bf20f630e37ddf4c497b546ad8
Spencer-Smith <spencer.smith2012@gmail.com>
2018-02-09 19:19:45 -0700

    Updated model facade classes...
    for basic response to operations

--- src/tickets/client/ModelFacade.java
+++ src/tickets/client/ModelFacade.java
@@ -6,7 +6,7 @@ import tickets.common.response.*;
 import tickets.client.model.ClientModelRoot;
 
 public class ModelFacade {
-  //Singleton structure
+	//Singleton structure
 	public static ModelFacade INSTANCE = null;
 	public static ModelFacade getInstance() {
 		if (INSTANCE == null)
@@ -14,85 +14,98 @@ public class ModelFacade {
 		return INSTANCE;
 	}
 	
-  //variables
-  private ClientModelRoot modelRoot = new ClientModelRoot();
+	//variables
+	private ClientModelRoot modelRoot = new ClientModelRoot();
 
-  //methods
-  public boolean register(UserData userData) throws Exception {
-    LoginResponse result = ServerProxy.getInstance().register(userData);
+	//methods
+	public boolean register(UserData userData) throws Exception {
+		LoginResponse result = ServerProxy.getInstance().register(userData);
 
-    if (result.getException() == null) {
-      System.out.println(result.getAuthToken());
-      modelRoot.setUserData(userData);
-      modelRoot.addAuthenticationToken(result.getAuthToken());
-      //TODO: Figure out how to use StateChange to notify presenters of login
-      //  modelRoot.updateObserable( ... )
-      return true;
-    }
-    else {
-      throw result.getException();
-    }
-  }
+		if (result.getException() == null) {
+			System.out.println(result.getAuthToken());
+			modelRoot.setUserData(userData);
+			modelRoot.addAuthenticationToken(result.getAuthToken());
+			return true;
+		} else {
+			throw result.getException();
+		}
+	}
 
-  public boolean login(UserData userData) throws Exception {
-	LoginResponse result = ServerProxy.getInstance().login(userData);
+	public boolean login(UserData userData) throws Exception {
+		LoginResponse result = ServerProxy.getInstance().login(userData);
     
-	if (result.getException() == null) {
-	  modelRoot.setUserData(userData);
-	  modelRoot.addAuthenticationToken(result.getAuthToken());
-      return true;
-    }
-    else {
-      throw result.getException();
-    }
-  }
+		if (result.getException() == null) {
+			modelRoot.setUserData(userData);
+			modelRoot.addAuthenticationToken(result.getAuthToken());
+			return true;
+		} else {
+			throw result.getException();
+		}
+	}
 
-  public boolean joinLobby(String id) {
-	  JoinLobbyResponse result = ServerProxy.getInstance().joinLobby(id);
+	public boolean joinLobby(String id) throws Exception {
+		JoinLobbyResponse result = ServerProxy.getInstance().joinLobby(id);
 	    
 		if (result.getException() == null) {
 	      return true;
+	    } else {
+	      throw result.getException();
 	    }
-	    else {
-	      return false;
-	    }
-  }
+	}
 
-  public boolean createLobby(Lobby lobby) {
-	  JoinLobbyResponse result = ServerProxy.getInstance().createLobby(lobby);
+	public boolean createLobby(Lobby lobby) throws Exception {
+		JoinLobbyResponse result = ServerProxy.getInstance().createLobby(lobby);
 	    
 		if (result.getException() == null) {
 	      return true;
+	    } else {
+	      throw result.getException();
 	    }
-	    else {
-	      return false;
-	    }
-  }
+	}
 
-  public boolean logout() {
-	  LogoutResponse result = ServerProxy.getInstance().logout();
+	public boolean logout() throws Exception {
+		LogoutResponse result = ServerProxy.getInstance().logout();
 	    
 		if (result.getException() == null) {
 	      return true;
+	    } else {
+	      throw result.getException();
 	    }
-	    else {
-	      return false;
-	    }
-  }
+	}
 
-  public boolean startGame(Lobby id) {
-    return false;
-  }
+	public boolean startGame(String lobbyId) throws Exception {
+		StartGameResponse result = ServerProxy.getInstance().startGame(lobbyId);
+		if (result.getException() == null) {
+			return true;
+		} else {
+			throw result.getException();
+		}
+	}
 
-  public boolean leaveLobby(UserData user) {
-    return false;
-  }
+	public boolean leaveLobby(String lobbyId) throws Exception {
+		LeaveLobbyResponse result = ServerProxy.getInstance().leaveLobby(lobbyId);
+		if (result.getException() == null) {
+			return true;
+		} else {
+			throw result.getException();
+		}
+	}
 
-  public boolean addGuest(Lobby id) {
-    return false;
-  }
+	public boolean addGuest(String lobbyId) throws Exception {
+		AddGuestResponse result = ServerProxy.getInstance().addGuest(lobbyId);
+		if (result.getException() == null) {
+			return true;
+		} else {
+			throw result.getException();
+		}
+	}
 
-  public boolean takeTurn(String playerId) {
-    return false;
-  }
+	public boolean takeTurn(String playerId) throws Exception {
+		PlayerTurnResponse result = ServerProxy.getInstance().takeTurn(playerId);
+		if (result.getException() == null) {
+			return true;
+		} else {
+			throw result.getException();
+		}
+	}
 }
