e88b8b94f7bd1c7b9d3c0caecbaf68cb3ce955e2
pultilian <pultilian@gmail.com>
2018-03-03 15:07:28 -0700

    Final Hopefully
    Please work

--- app/src/main/java/tickets/client/async/StartGameAsync.java
+++ app/src/main/java/tickets/client/async/StartGameAsync.java
@@ -14,6 +14,7 @@ import tickets.client.ModelFacade;
 import tickets.common.response.StartGameResponse;
 
 
+class StartGameAsync extends AsyncTask<String, Void, StartGameResponse> {
     ModelFacade modelRoot;
 
     public StartGameAsync(ModelFacade root) {
@@ -21,19 +22,33 @@ import tickets.common.response.StartGameResponse;
     }
 
     @Override
+    public StartGameResponse doInBackground(String... data) {
         if (data.length != 2) {
             AsyncException error = new AsyncException(this.getClass(), "invalid execute() parameters");
+            return new StartGameResponse(error);
         }
 
         String id = data[0];
         String authToken = data[1];
 
+        StartGameResponse response = ServerProxy.getInstance().startGame(id, authToken);
         return response;
     }
 
     @Override
         if (response == null) {
             Exception ex = new Exception("The Server could not be reached");
+            ExceptionMessage msg = new ExceptionMessage(ex);
+            modelRoot.updateObservable(msg);
+        }
+        else if (response.getException() == null) {
+            ClientStateChange.ClientState stateVal;
+            stateVal = ClientStateChange.ClientState.game;
+            ClientStateChange state = new ClientStateChange(stateVal);
+            modelRoot.updateObservable(state);
+        }
+        else {
+            Exception ex = response.getException();
             ExceptionMessage msg = new ExceptionMessage(ex);
             modelRoot.updateObservable(msg);
         }
