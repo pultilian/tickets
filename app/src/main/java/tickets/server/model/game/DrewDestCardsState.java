d55d10fcc67f9b90e832a54c79407dd1c5278fd9
Will Pinkston <william@pinkston.com>
2018-04-18 22:44:21 -0600

    fixed some bugs within dataaccess and deserialization of state pattern

--- app/src/main/java/tickets/server/model/game/DrewDestCardsState.java
+++ app/src/main/java/tickets/server/model/game/DrewDestCardsState.java
@@ -14,80 +14,82 @@ import tickets.common.Route;
 // At this state, the player has drawn three destination cards
 //	 and must now choose whether to discard two, one or zero of them.
 //
-class DrewDestCardsState extends PlayerTurnState {
+public class DrewDestCardsState extends PlayerTurnState {
+	private String drewDestCardsStateFlag = "flag";
 
-	// Singleton pattern
-	private static DrewDestCardsState INSTANCE;
+    // Singleton pattern
+    private static DrewDestCardsState INSTANCE;
 
-	static DrewDestCardsState getInstance() {
-		if (INSTANCE == null) {
-			INSTANCE = new DrewDestCardsState();
-		}
-		return INSTANCE;
-	}
-
-	private boolean turn0;
-	private DrewDestCardsState() {
-	    turn0 = true;
+    public static DrewDestCardsState getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new DrewDestCardsState();
+        }
+        return INSTANCE;
     }
 
-	@Override
-	String drawTrainCard(TrainCard card, ServerPlayer player) {
-		return "You must choose which destination cards (if any) to discard.";
-	}
+    private boolean turn0;
 
-	@Override
-	String drawFaceUpCard(TrainCard card, ServerPlayer player) {
-		return "You must choose which destination cards (if any) to discard.";
-	}
+    private DrewDestCardsState() {
+        turn0 = true;
+    }
 
-	@Override
-	String claimRoute(Route route, List<TrainCard> cards, ServerPlayer player) {
-		return "You must choose which destination cards (if any) to discard.";
-	}
+    @Override
+    String drawTrainCard(TrainCard card, ServerPlayer player) {
+        return "You must choose which destination cards (if any) to discard.";
+    }
 
-	@Override
-	String drawDestinationCards(List<DestinationCard> cards, ServerPlayer player) {
-		return "You must choose which destination cards (if any) to discard.";
-	}
+    @Override
+    String drawFaceUpCard(TrainCard card, ServerPlayer player) {
+        return "You must choose which destination cards (if any) to discard.";
+    }
 
-	@Override
-	String discardDestinationCard(List<DestinationCard> cards, ServerPlayer player) {
-		if (turn0 && cards.size() > 1)
-		    return "You may only discard one card during setup";
-		else if (!turn0 && cards.size() > 2)
-		    return "You may only discard up to two cards";
+    @Override
+    String claimRoute(Route route, List<TrainCard> cards, ServerPlayer player) {
+        return "You must choose which destination cards (if any) to discard.";
+    }
+
+    @Override
+    String drawDestinationCards(List<DestinationCard> cards, ServerPlayer player) {
+        return "You must choose which destination cards (if any) to discard.";
+    }
+
+    @Override
+    String discardDestinationCard(List<DestinationCard> cards, ServerPlayer player) {
+        if (turn0 && cards.size() > 1)
+            return "You may only discard one card during setup";
+        else if (!turn0 && cards.size() > 2)
+            return "You may only discard up to two cards";
 
 
-		List<DestinationCard> options = player.getDestinationCardOptions();
-		List<DestinationCard> toDiscard = new ArrayList<>();
+        List<DestinationCard> options = player.getDestinationCardOptions();
+        List<DestinationCard> toDiscard = new ArrayList<>();
 
-		// Remove all discarded cards
-		for (DestinationCard card : cards) {
-			if (card != null) {
-				for (DestinationCard option : options) {
-					if (card.equals(option)) toDiscard.add(option);
-				}
-			}
-		}
-		options.removeAll(toDiscard);
+        // Remove all discarded cards
+        for (DestinationCard card : cards) {
+            if (card != null) {
+                for (DestinationCard option : options) {
+                    if (card.equals(option)) toDiscard.add(option);
+                }
+            }
+        }
+        options.removeAll(toDiscard);
 
-		// Set player's destination card options to the kept cards
-		ChoiceDestinationCards keptCards = new ChoiceDestinationCards();
-		keptCards.setDestinationCards(options);
-		player.setDestinationCardOptions(keptCards);
+        // Set player's destination card options to the kept cards
+        ChoiceDestinationCards keptCards = new ChoiceDestinationCards();
+        keptCards.setDestinationCards(options);
+        player.setDestinationCardOptions(keptCards);
 
-		// Add all kept cards to the player's hand
-		for (DestinationCard card : player.getDestinationCardOptions()) {
-			player.addDestinationCardToHand(card);
-		}
+        // Add all kept cards to the player's hand
+        for (DestinationCard card : player.getDestinationCardOptions()) {
+            player.addDestinationCardToHand(card);
+        }
         turn0 = false;
         player.changeState(States.NOT_MY_TURN);
-		return null;
-	}
+        return null;
+    }
 
-	@Override
-	String endTurn(ServerPlayer player) {
-		return "You must choose which destination cards (if any) to discard.";
-	}
+    @Override
+    String endTurn(ServerPlayer player) {
+        return "You must choose which destination cards (if any) to discard.";
+    }
 }
